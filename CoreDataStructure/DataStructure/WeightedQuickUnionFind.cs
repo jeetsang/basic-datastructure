8aa1e7bd048e2d0e9ddd44d13e21dac0eb176a20
vaishnavjitendra <jeetsangjitendra@gmail.com>
2015-03-06 00:06:42 +0530

    Weighted Quick Union Find

---
+++ CoreDataStructure/DataStructure/WeightedQuickUnionFind.cs
@@ -0,0 +1,49 @@
+﻿namespace DataStructure
+{
+    public class WeightedQuickUnionFind
+    {
+        private int[] id;
+        private int[] size;
+
+        public WeightedQuickUnionFind(int N)
+        {
+            id = new int[N];
+            size =new int[N];
+            for (var i = 0; i < N; i++)
+            {
+                id[i] = i;
+                size[i] = 1;
+            }
+        }
+
+        private int Root(int i)
+        {
+            while (i!=id[i])
+            {
+                i = id[i];
+            }
+            return i;
+        }
+
+        public bool Connected(int p, int q)
+        {
+            return Root(p) == Root(q);
+        }
+
+        public void Union(int p, int q)
+        {
+            var i = Root(p);
+            var j = Root(q);
+            if (size[i] > size[j])
+            {
+                id[j] = i;
+                size[i] = size[i] + size[j];
+            }
+            else
+            {
+                id[i] = j;
+                size[j] = size[j] + size[i];
+            }
+        }
+    }
+}
